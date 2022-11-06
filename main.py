4dbe05e4a62ac9eaaa02959dc1c6b2e0b2a570e6
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2022-11-06 10:09:53 -0800

    Added table

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from docx import Document
 from docx.shared import RGBColor
 import re
 import xlwings
+from tkinter import *
+from tkinter import filedialog
 
 # DER and TBV are not valid tags
 docRelation = {"HRD":("HRS"), "HRS":("PRS"), "PRS":("URS","RISK"), "HTR":("HTP"), "HTP":("HRD", "HRS"), \
@@ -58,7 +60,7 @@ def GetParentTags():                    # Returns only valid parent tags
                     #red.font.color.rgb = RGBColor(255, 0, 0)
 
                     #print(y[0])
-                    qe.append(y[0])
+                    qe.append(y[0])#adds to parent tag list
                 index = index + 1
             # print(ind)
             else:
@@ -71,7 +73,7 @@ def GetParentTags():                    # Returns only valid parent tags
                     #red.bold = True
                     #red.font.color.rgb = RGBColor(255, 0, 0)
                     #print(y[0])
-                    qe.append(y[0])
+                    qe.append(y[0]) #adds to parent tag list
                 index = index + 1
 
             #print(ind)
@@ -93,7 +95,7 @@ def GetChildTags():                     # Returns only valid child tags
                         #green.font.color.rgb = RGBColor(0x00, 0xFF, 0x00)
                         #green.bold = True
                         #print(y[0])
-                        qa.append(y[0])
+                        qa.append(y[0])#adds to child tag list
                 index = index + 1
                 # print(ind)
             else:
@@ -107,7 +109,7 @@ def GetChildTags():                     # Returns only valid child tags
                         #green.font.color.rgb = RGBColor(0x00, 0xFF, 0x00)
                         #green.bold = True
                         #print(y[0])
-                        qa.append(y[0])
+                        qa.append(y[0]) #adds to child tag list
                 index = index + 1
 
             #print(ind)
@@ -129,10 +131,10 @@ def GetOrphanTags():
                 #green.bold = True
 
                 #print(y[0])
-                q.append(y[0])
+                q.append(y[0]) #adds to orphan tag list
                 index = index + 1
 
-                # print(ind)
+                #print(ind)
 
             #else:
             #    if re.search('.*[:\s]' + re.escape(tag) + '[:\s]', t):
@@ -148,9 +150,9 @@ def GetOrphanTags():
             #    index = index + 1
             # print(ind)
 
-qe = []
-qa = []
-q = []
+qe = [] #list of parent tags
+qa = [] #List of cild tags
+q = [] #List for orphans
 #runner2 = paragraph.add_run("\n\nParent tag/tags\n\n")
 #runner2.bold = True                              #make it bold
 GetParentTags()
@@ -158,29 +160,69 @@ GetParentTags()
 #runner2 = paragraph.add_run("\n\nChild tag/tags\n\n")
 #runner2.bold = True
 GetChildTags()
-print(qe) # Parent tags
-print(qa) #Child tags
+print(qe) # prints Parent tags
+print(qa) #prints Child tags
+GetOrphanTags()
 #print(q)
-num = 0
-while qe and qa:
-    green = paragraph.add_run(qe[0] + "    ")
+#num = 0
+#while qe and qa:
+    #green = paragraph.add_run(qe[0] + "    ")
     #paragraph.add_run("\n\n")
-    green.font.color.rgb = RGBColor(0x00, 0xFF, 0x00)
-    green.bold = True
+    #green.font.color.rgb = RGBColor(0x00, 0xFF, 0x00)
+    #green.bold = True
+    #qe.remove(qe[0])
+    #red = paragraph.add_run(qa[0])
+    #paragraph.add_run("\n\n")
+    #red.bold = True
+    #red.font.color.rgb = RGBColor(255, 0, 0)
+    #qa.remove(qa[0])
+
+#while qe:
+    #green = paragraph.add_run(qe[0])
+    #paragraph.add_run("\n\n")
+    #green.font.color.rgb = RGBColor(0x00, 0xFF, 0x00)
+    #green.bold = True
+    #qe.remove(qe[0])
+
+table = report3.add_table(rows=1, cols=2)
+
+# Adding heading in the 1st row of the table
+row = table.rows[0].cells
+row[0].text = 'Parent Tag'
+row[1].text = 'Child Tag/Tags'
+
+
+
+# Adding style to a table
+table.style = 'Colorful List'
+
+
+while qe and qa:
+    row = table.add_row().cells # Adding a row and then adding data in it.
+    row[0].text = qe[0]
+    #green = paragraph.add_run(sentences[0] + "    ")
+    #paragraph.add_run("\n\n")
+    #green.font.color.rgb = RGBColor(0x00, 0xFF, 0x00)
+    #green.bold = True
     qe.remove(qe[0])
-    red = paragraph.add_run(qa[0])
-    paragraph.add_run("\n\n")
-    red.bold = True
-    red.font.color.rgb = RGBColor(255, 0, 0)
+    #red = paragraph.add_run(child[0])
+    row[1].text = qa[0]
+    #paragraph.add_run("\n\n")
+    #red.bold = True
+    #red.font.color.rgb = RGBColor(255, 0, 0)
     qa.remove(qa[0])
 
 while qe:
-    green = paragraph.add_run(qe[0])
-    paragraph.add_run("\n\n")
-    green.font.color.rgb = RGBColor(0x00, 0xFF, 0x00)
-    green.bold = True
+    row[0].text = qe[0]
+    #green = paragraph.add_run(sentences[0])
+    #paragraph.add_run("\n\n")
+    #green.font.color.rgb = RGBColor(0x00, 0xFF, 0x00)
+    #green.bold = True
     qe.remove(qe[0])
 
+
+#paragraph.add_run(f)
+report3.save('report3.docx')
 
 
 
